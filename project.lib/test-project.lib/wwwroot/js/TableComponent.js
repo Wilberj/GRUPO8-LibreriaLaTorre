0618458281106484598b7ef4a0d68c3958aaf2b5
alejandrathv <alejandratltl@hotmail.com>
2022-06-26 20:52:06 -0600

    cambios capa UI

--- project.lib/test-project.lib/wwwroot/js/TableComponent.js
+++ project.lib/test-project.lib/wwwroot/js/TableComponent.js
@@ -1,1 +1,34 @@
-﻿
+﻿const thead = Table.querySelector("thead");
+const tbody = Table.querySelector("tbody");
+thead.innerHTML = "";
+tbody.innerHTML = "";
+Dataset.forEach((Item, Index) => {
+    const row = document.createElement("tr");
+    for (var prop in Item) {
+        const td = document.createElement("td");
+        td.innerHTML = Item[prop];
+        row.append(td);
+        if (Index == 0) {
+            const th = document.createElement("th");
+            th.innerText = prop;
+            thead.append(th);
+        }
+    }
+    if (Index == 0) {
+        const th = document.createElement("th");
+        th.innerText = "actions";
+        thead.append(th);
+    }
+    const tdAction = document.createElement("td");
+    const btn = document.createElement("input");
+    btn.className = "BtnOK";
+
+    btn.type = "Button";
+    btn.value = BtnValue;
+    btn.onclick = () => {
+        TableFunction(Item);
+    }
+    tdAction.append(btn);
+    row.append(tdAction);
+    tbody.append(row);
+});
